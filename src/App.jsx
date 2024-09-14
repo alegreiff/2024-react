52099d1e742a61f6732f03bdba0f70ca97d7ebd6
Jaime de Greiff <alegreiff@gmail.com>
2024-09-13 20:27:29 -0500

    Inicio

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+function Header() {
+  return (
+    <>
+      <header>
+        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <h1>Jaime de Greiff @alegreiff</h1>
+        <p>
+          Conceptos básicos de react que son necesario s para desarrollar un app
+        </p>
+      </header>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h2>Empezamos porque se hace tarde</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
