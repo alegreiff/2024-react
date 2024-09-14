2f07336b826ab482362a01e143825d18f31b6f43
Jaime de Greiff <alegreiff@gmail.com>
2024-09-13 20:58:56 -0500

    images paths components and imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import image1 from "/assets/jsx-ui.png";
+const reactDescriptions = ["CLAVES", "BÁSICOS", "IMPRESCINDIBLES"];
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
 function Header() {
+  const description = reactDescriptions[getRandomInt(3)];
   return (
     <>
       <header>
-        <img src="/assets/react-core-conceptss.png" alt="Stylized atom" />
+        <img src={image1} alt="Stylized atom" />
         <h1>Jaime de Greiff @alegreiff</h1>
         <p>
-          Conceptos básicos de react que son necesario s para desarrollar un app
+          Conceptos {description} de react que son necesarios para desarrollar
+          un app
         </p>
       </header>
     </>
