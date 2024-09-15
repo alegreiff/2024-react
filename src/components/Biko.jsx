a792442331fbfa83b27d2ddb33eacbc464ff9e5a
Jaime de Greiff <alegreiff@gmail.com>
2024-09-15 10:40:22 -0500

    powerbiiframe

--- src/components/Biko.jsx
+++ src/components/Biko.jsx
@@ -2,6 +2,7 @@ export const Biko = () => {
   return (
     <div className="bg-teal-400 text-white p-4">
       <h2>Biko negro</h2>
+      <iframe title="ensayo-ga" width="100%" height="420" src="https://app.powerbi.com/view?r=eyJrIjoiYzYxMjcwMmMtZWFhMy00MWQ3LWE2ODEtZTE1ZGE2ZjdmOTgwIiwidCI6IjkzZjI4YjRlLTkzNWMtNGMxNC04OGFjLWU0ODY5N2MxZmM5NyJ9" frameborder="0" allowFullScreen="true"></iframe>
     </div>
   );
 };
